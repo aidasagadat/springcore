39bd260a3eb60d5408d56d1130407c7ae65982f1
Aida Sagadat <aidasagadat355@gmail.com>
2024-03-28 21:57:53 +0500

    Add @AutoWired annotation

--- src/main/java/com/lovetocode/springcoredemo/DemoController.java
+++ src/main/java/com/lovetocode/springcoredemo/DemoController.java
@@ -1,11 +1,13 @@
 package com.lovetocode.springcoredemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     private  Coach theCoach;
 
+    @Autowired
     public DemoController(Coach theCoach){
         theCoach = theCoach;
     }
