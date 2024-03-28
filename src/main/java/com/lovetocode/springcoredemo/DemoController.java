1b46c32d588aae1ee03cf1c1cadfd992240ed0b2
Aida Sagadat <aidasagadat355@gmail.com>
2024-03-28 22:02:24 +0500

    Add @GetMapping for /dailyworkout

--- src/main/java/com/lovetocode/springcoredemo/DemoController.java
+++ src/main/java/com/lovetocode/springcoredemo/DemoController.java
@@ -1,6 +1,7 @@
 package com.lovetocode.springcoredemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,11 @@ public class DemoController {
     public DemoController(Coach theCoach){
         theCoach = theCoach;
     }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return theCoach.getDailyWorkout();
+    }
+
 
 }
